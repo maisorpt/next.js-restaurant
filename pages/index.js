d0c35a38dba5f32974ffbbe17980764a2718213f
maisorpt <maisorpt@gmail.com>
2023-05-12 08:44:55 +0700

    next

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import RestaurantList from "../components/RestaurantList";
+
+export default function Home({ restaurants }) {
+  return (
+    <div className='container'>
+      <RestaurantList restaurants={restaurants} />
+    </div>
+  )
+};
+export async function getStaticProps() {
+  const response = await fetch('https://restaurant-api.dicoding.dev/list');
+  const { restaurants } = await response.json();
+ 
+  return {
+    props: {
+      restaurants,
+    },
+  }
+}
